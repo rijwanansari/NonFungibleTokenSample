6580101be8487a778e67adda42a36400a017e11d
Rijwan Ansari <rijwan@techoneglobal.com>
2023-03-15 21:17:22 +0800

    Add project files.

---
+++ INonFungibleTokenMetadata.cs
@@ -0,0 +1,13 @@
+﻿using Stratis.SmartContracts;
+
+namespace NFTContract
+{
+    public interface INonFungibleTokenMetadata
+    {
+        string Name { get; }
+        string Symbol { get; }
+
+        string TokenURI(UInt256 tokenId);
+
+    }
+}
---
+++ ISupportsInterface.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace NFTContract
+{
+    public interface ISupportsInterface
+    {
+        /// <summary>
+        /// Function to check which interfaces are supported by this contract.
+        /// </summary>
+        /// <param name="interfaceID">Id of the interface.</param>
+        /// <returns>True if <see cref="interfaceID"/> is supported, false otherwise.</returns>
+        bool SupportsInterface(uint interfaceID);
+    }
+}
